ddc44be4fa83b3dc4fd3c886934e0aa74ce8ce9b
kanngji <alleneffort1994@gmail.com>
2023-05-12 04:37:19 +0900

    페이지네이션 맨 처음, 맨 끝 구현

--- board/views.py
+++ board/views.py
@@ -3,11 +3,19 @@ from django.http import HttpResponseNotAllowed
 from django.utils import timezone
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+
+last_page_num = 0
+
 # Create your views here.
 
 def index(request):
+    page = request.GET.get('page','1') # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list':question_list}
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    max_index = len(paginator.page_range)
+    page_obj = paginator.get_page(page)
+    context = {'question_list':page_obj,'max_index':max_index}
     return render(request, 'board/question_list.html',context)
 
 # 질문 상세
