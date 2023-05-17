8ebeb44e7bda7ce8bc25a97786f7570b0877975e
kanngji <alleneffort1994@gmail.com>
2023-05-18 01:31:47 +0900

    글쓴이표시 , @login 어노테이션

--- board/views.py
+++ board/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from django.http import HttpResponseNotAllowed
+
 from django.utils import timezone
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 last_page_num = 0
 
@@ -25,26 +26,30 @@ def detail(request,question_id):
     return render(request, 'board/question_detail.html',context)
 
 # 답변 달기
+@login_required(login_url='common:login')
 def answer_create(request,question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method=='POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('board:detail',question_id=question_id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible')
+        form = AnswerForm()
     context = {'question':question, 'form':form}
     return render(request,'board/question_detail.html',context)
 # 질문 생성
+@login_required
 def question_create(request):
     if request.method=='POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('board:index')
