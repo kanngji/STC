be50bbf5e54b8d0caf70935bf6341a4797ff674b
kanngji <alleneffort1994@gmail.com>
2023-05-27 01:21:14 +0900

    게시판 검색 구현

--- board/views/base_views.py
+++ board/views/base_views.py
@@ -1,15 +1,25 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404,redirect
+from django.db.models import Q
 
 from ..models import Question
 last_page_num = 0
 def index(request):
     page = request.GET.get('page','1') # 페이지
+    kw = request.GET.get('kw','') # 검색어
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
     max_index = len(paginator.page_range)
     page_obj = paginator.get_page(page)
-    context = {'question_list':page_obj,'max_index':max_index}
+    context = {'question_list':page_obj,'max_index':max_index, 'page':page,'kw':kw}
     return render(request, 'board/question_list.html',context)
 
 # 질문 상세
